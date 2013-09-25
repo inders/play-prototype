afe4b5ba1b41f1a6fe74193c7c92af97b9345bad
inderbir.singh <inderbir.singh@inmobi.com>
2013-09-25 18:43:44 +0530

    Interactive typeahead base on response from server

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import org.codehaus.jackson.node.ObjectNode;
 import play.data.*;
 import play.libs.Json;
 import play.mvc.*;
@@ -21,10 +22,38 @@ public class Application extends Controller {
         );
     }
 
+    private class QueryResponse {
+        String key = "result";
+        String response = "inder";
+
+        private QueryResponse(String key, String response) {
+            this.key = key;
+            this.response = response;
+        }
+
+        private String getKey() {
+            return key;
+        }
+
+        private String getResponse() {
+            return response;
+        }
+    }
+
     public static Result query(String query){
+
         System.out.println("Query " + query);
         System.out.println("Response " + Json.toJson("inder"));
-        return Results.ok(Json.toJson("inder"));
+        ObjectNode result = Json.newObject();
+        if (query.contains("inmobi")) {
+          result.put("Response", "inmobi solutions private limited");
+        } else  if (query.contains("google")) {
+            result.put("Response", "Google Inc");
+
+        }
+
+
+        return Results.ok(Json.toJson(result));
     }
 
     public static Result newTask() {
